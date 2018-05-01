94a095ef40e3ec78b0b9bb4ff6efe1adbac56cc2
z3r0-0112 <34434717+z3r0-0112@users.noreply.github.com>
2018-05-01 20:44:26 +0700

    Update Member.h

--- Lab_Management/Headers/Member.h
+++ Lab_Management/Headers/Member.h
@@ -3,12 +3,13 @@
 class Member {
 private:
 	std::string name;
-	std::string age;
-	std::string phonenumber;
 	std::string ID_number;
+	std::string Class;
+	std::string email;
+	std::string phonenumber;
 public:
 	Member();
-	void add_member(std::string name, std::string age, std::string phonenumber, std::string ID_number);
+	void add_member(std::string name, std::string ID_number, std::string Class, std::string email, std::string phonenumber);
 	void delete_member();
 	void Update();
 	void print_member_info();
